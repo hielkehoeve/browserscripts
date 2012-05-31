2e26295ea340b7235fee83fdd51724cd0f84e9ff
Hielke Hoeve <hielke.hoeve@topicus.nl>
2012-05-31 14:37:58 +0200

    add githubabsolutetime.js

---
+++ githubabsolutetime.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name       GitHub absolute time
+// @namespace  nl.topicus
+// @version    0.3.1
+// @description  renders the absolute time instead of the relative time
+// @match      https://github.com/*
+// @copyright  2012+, Hielke Hoeve
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// ==/UserScript==
+ 
+function pad(n) 
+{
+    return n < 10 ? '0' + n : n 
+}
+  
+$('.commit-group time').each(function()
+{
+    var date = new Date($(this).attr('datetime'));
+    $(this).text(pad(date.getUTCHours())+":"+pad(date.getUTCMinutes()));
+});
+  
+$('.tree-browser time, .full-commit time, .commit-tease time').each(function()
+{
+    var date = new Date($(this).attr('datetime'));
+    $(this).text(date.getUTCDate()+"-"+date.getUTCMonth()+"-"+date.getUTCFullYear()+" "+pad(date.getUTCHours())+":"+pad(date.getUTCMinutes()));
+});
+
