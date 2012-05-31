6810c16dd3d8e4bc727190d27469954650967992
Jeroen Idserda <jeroen.idserda@topicus.nl>
2012-05-31 15:01:14 +0200

    Display time in local timezone
    Conflicts:
    
    	githubabsolutetime.js

--- githubabsolutetime.js
+++ githubabsolutetime.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       GitHub absolute time
 // @namespace  nl.topicus
-// @version    0.3.1
+// @version    0.3.2
 // @description  renders the absolute time instead of the relative time
 // @match      https://github.com/*
 // @copyright  2012+, Hielke Hoeve
@@ -13,19 +13,25 @@ function GitHubAbsoluteTimePad(n)
     return n < 10 ? '0' + n : n 
 }
 
+function GitHubAbsoluteTimeLocalTime(date) {
+    var localOffset = date.getTimezoneOffset() * (60*1000);
+    var localTime = date.getTime();
+    return new Date(localTime - localOffset);  
+}
+
 function GitHubAbsoluteTimeRender()
 {
     console.log("Absolute time rendered on GitHub page: "+new Date());
     
     $('.commit-group time').each(function()
     {
-        var date = new Date($(this).attr('datetime'));
+        var date = GitHubAbsoluteTimeLocalTime(new Date($(this).attr('datetime')));
         $(this).text(GitHubAbsoluteTimePad(date.getUTCHours())+":"+GitHubAbsoluteTimePad(date.getUTCMinutes()));
     });
   
     $('.tree-browser time, .full-commit time, .commit-tease time, .file-history-tease time').each(function()
     {
-        var date = new Date($(this).attr('datetime'));
+        var date = GitHubAbsoluteTimeLocalTime(new Date($(this).attr('datetime')));
         $(this).text(date.getUTCDate()+"-"+date.getUTCMonth()+"-"+date.getUTCFullYear()+" "+GitHubAbsoluteTimePad(date.getUTCHours())+":"+GitHubAbsoluteTimePad(date.getUTCMinutes()));
     });
     
