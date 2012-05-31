10b1ee88db8a1fa5eb01f9a5e7546c8bedb89e09
Hielke Hoeve <hielke.hoeve@gmail.com>
2012-05-31 18:04:14 +0300

    run this script every 30 seconds to account for github background updates.

--- githubabsolutetime.js
+++ githubabsolutetime.js
@@ -1,27 +1,35 @@
 // ==UserScript==
 // @name       GitHub absolute time
 // @namespace  nl.topicus
-// @version    0.3.2
+// @version    0.3.1
 // @description  renders the absolute time instead of the relative time
 // @match      https://github.com/*
 // @copyright  2012+, Hielke Hoeve
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // ==/UserScript==
  
-function pad(n) 
+function GitHubAbsoluteTimePad(n) 
 {
     return n < 10 ? '0' + n : n 
 }
-  
-$('.commit-group time').each(function()
-{
-    var date = new Date($(this).attr('datetime'));
-    $(this).text(pad(date.getUTCHours())+":"+pad(date.getUTCMinutes()));
-});
-  
-$('.tree-browser time, .full-commit time, .commit-tease time, .file-history-tease time').each(function()
-{
-    var date = new Date($(this).attr('datetime'));
-    $(this).text(date.getUTCDate()+"-"+date.getUTCMonth()+"-"+date.getUTCFullYear()+" "+pad(date.getUTCHours())+":"+pad(date.getUTCMinutes()));
-});
 
+function GitHubAbsoluteTimeRender()
+{
+    console.log("Absolute time rendered on GitHub page: "+new Date());
+    
+    $('.commit-group time').each(function()
+    {
+        var date = new Date($(this).attr('datetime'));
+        $(this).text(GitHubAbsoluteTimePad(date.getUTCHours())+":"+GitHubAbsoluteTimePad(date.getUTCMinutes()));
+    });
+  
+    $('.tree-browser time, .full-commit time, .commit-tease time, .file-history-tease time').each(function()
+    {
+        var date = new Date($(this).attr('datetime'));
+        $(this).text(date.getUTCDate()+"-"+date.getUTCMonth()+"-"+date.getUTCFullYear()+" "+GitHubAbsoluteTimePad(date.getUTCHours())+":"+GitHubAbsoluteTimePad(date.getUTCMinutes()));
+    });
+    
+    setTimeout(GitHubAbsoluteTimeRender, 30000);
+}
+
+GitHubAbsoluteTimeRender();
