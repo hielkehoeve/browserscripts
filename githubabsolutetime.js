4b2e37dd16eaf3f2b43e692b40f6c42220cc7c09
Hielke Hoeve <hielke.hoeve@topicus.nl>
2012-06-06 09:53:44 +0200

    render the month +1 (silly js impl counts from 0 to 11), and pad the date and month if necessary

--- githubabsolutetime.js
+++ githubabsolutetime.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name       GitHub absolute time
 // @namespace  nl.topicus
-// @version    0.3.2
+// @version    0.3.3
 // @description  renders the absolute time instead of the relative time
 // @match      https://github.com/*
 // @copyright  2012+, Hielke Hoeve
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // ==/UserScript==
  
-function GitHubAbsoluteTimePad(n) 
+function GitHubAbsolutePad(n) 
 {
     return n < 10 ? '0' + n : n 
 }
@@ -26,16 +26,17 @@ function GitHubAbsoluteTimeRender()
     $('.commit-group time').each(function()
     {
         var date = GitHubAbsoluteTimeLocalTime(new Date($(this).attr('datetime')));
-        $(this).text(GitHubAbsoluteTimePad(date.getUTCHours())+":"+GitHubAbsoluteTimePad(date.getUTCMinutes()));
+        $(this).text(GitHubAbsolutePad(date.getUTCHours())+":"+GitHubAbsolutePad(date.getUTCMinutes()));
     });
   
     $('.tree-browser time, .full-commit time, .commit-tease time, .file-history-tease time').each(function()
     {
         var date = GitHubAbsoluteTimeLocalTime(new Date($(this).attr('datetime')));
-        $(this).text(date.getUTCDate()+"-"+date.getUTCMonth()+"-"+date.getUTCFullYear()+" "+GitHubAbsoluteTimePad(date.getUTCHours())+":"+GitHubAbsoluteTimePad(date.getUTCMinutes()));
+        $(this).text(GitHubAbsolutePad(date.getUTCDate())+"-"+GitHubAbsolutePad(date.getUTCMonth()+1)+"-"+date.getUTCFullYear()+" "+GitHubAbsolutePad(date.getUTCHours())+":"+GitHubAbsolutePad(date.getUTCMinutes()));
     });
     
     setTimeout(GitHubAbsoluteTimeRender, 30000);
 }
 
 GitHubAbsoluteTimeRender();
+
