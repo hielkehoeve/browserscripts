39bbbdca1f8e8021f4fe4931e2e29802509efa34
Hielke Hoeve <hielkehoeve@users.noreply.github.com>
2015-04-30 10:36:56 +0200

    Create bhostedwordwrap.js

---
+++ bhostedwordwrap.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name       BHosted word wrap
+// @namespace  nl.topicus
+// @version    0.1
+// @description  BHosted word wrap
+// @match      https://klant.bhosted.nl/dns/index.php
+// @copyright  2015+, Hielke Hoeve
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// ==/UserScript==
+
+$(".tbl td:nth-child(3)").css("word-break", "break-word");
