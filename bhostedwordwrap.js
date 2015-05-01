11827bdbee738e99394107ce978f5cb77f549d7b
Hielke Hoeve <hielkehoeve@users.noreply.github.com>
2015-05-01 12:05:38 +0200

    Update bhostedwordwrap.js

--- bhostedwordwrap.js
+++ bhostedwordwrap.js
@@ -3,7 +3,7 @@
 // @namespace  nl.topicus
 // @version    0.1
 // @description  BHosted word wrap
-// @match      https://klant.bhosted.nl/dns/index.php
+// @match      https://klant.bhosted.nl/dns/*
 // @copyright  2015+, Hielke Hoeve
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // ==/UserScript==
